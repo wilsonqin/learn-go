bf1bb79cc435ab5e407a276789c3b302b4cb0b7a
Wilson Qin <wilson.qin@gmail.com>
2017-06-09 13:09:19 -0400

    added loops and functions example

---
+++ exercise_loops_and_functions.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func Sqrt(x float64) float64 {
+	z := 1.0
+	last_z := 0.0
+	
+	for math.Abs(z - last_z) >= 0000.1 {
+		last_z = z
+		z = last_z - ((math.Pow(last_z,2.) - x) / (2. * last_z))
+	}
+
+	return z
+}
+
+func main() {
+	for i := 1; i <= 10; i++ {
+		fmt.Printf("%v Squared: Guess %v,", i, Sqrt(float64(i)))
+		fmt.Printf(" Actual %v\n", math.Sqrt(float64(i)))
+	}
+}
+
