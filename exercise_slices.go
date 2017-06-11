279d28a1478d61aa1250bcfa8e2bb72e3e42ef08
Wilson Qin <wilson.qin@gmail.com>
2017-06-10 23:53:23 -0400

    using go slices exercise

---
+++ exercise_slices.go
@@ -0,0 +1,31 @@
+package main
+
+import "golang.org/x/tour/pic"
+
+func SwirlPic(dx, dy int) [][]uint8 {
+        matrix := make([][]uint8, dy)
+        for i := range matrix {
+                matrix[i] = make([]uint8, dx)
+                for j := range matrix[i] {
+                        matrix[i][j] = uint8(j * i - i - j)
+                }
+        }
+        return matrix
+}
+
+func GradientPic(dx, dy int) [][]uint8 {
+		matrix := make([][]uint8, dy)
+		for i := range matrix {
+				matrix[i] = make([]uint8, dx)
+				for j := range matrix[i] {
+						matrix[i][j] = uint8(i * dx + j)
+				}
+		}
+		return matrix
+}
+
+func run() {
+		pic.Show(GradientPic)
+}
+
+
