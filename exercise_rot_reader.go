b01d1d945aa70dc40d6baa6b3b7901c014a98cda
Wilson Qin <wilson.qin@gmail.com>
2017-07-06 15:57:43 -0400

    rot reader exercise

---
+++ exercise_rot_reader.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+    "io"
+    "os"
+    "strings"
+    "unicode"
+)
+
+type rot13Reader struct {
+    r io.Reader
+}
+
+func (r13 rot13Reader) Read(b []byte) (int, error) {
+    readAmt, error := r13.r.Read(b)
+    
+    for i := 0; i < readAmt; i++ {
+        s := rune(b[i])
+        if unicode.IsLetter(s){
+            switch {
+                case b[i] < 'n':
+                    b[i] += 13
+                case b[i] < 'A':
+                    b[i] -= 13
+                case b[i] < 'N':
+                    b[i] += 13
+                default:
+                    b[i] -= 13
+            }
+        }
+    }
+    
+    return len(b), error
+}
+
+func RunRot13Reader() {
+    s := strings.NewReader("Lbh penpxrq gur pbqr!")
+    r := rot13Reader{s}
+    io.Copy(os.Stdout, &r)
+}
--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 package main
 
 func main(){
-	RunMyReader()
+	RunRot13Reader()
 }
